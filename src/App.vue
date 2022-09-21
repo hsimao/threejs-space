349e3b8d7a672b4be6385be64de66848d1fce6d7
mars_front <mars_front@cxct.org>
2022-09-21 11:40:10 +0800

    feat: basic bg setting

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="screen" ref="screenDom" />
+
+    <div class="pages" ref="pages">
+      <div class="page">
+        <h2 class="title">page 1</h2>
+        <p>desc...</p>
+      </div>
+
+      <div class="page">
+        <h2 class="title">page 2</h2>
+        <p>desc...</p>
+      </div>
+
+      <div class="page">
+        <h2 class="title">page 3</h2>
+        <p>desc...</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import * as THREE from "three";
+import { ref, onMounted } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const screenDom = ref(null);
+
+onMounted(() => {
+  // 場景
+  const scene = new THREE.Scene();
+
+  // 相機
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  camera.position.set(0, 0, 10);
+
+  // 渲染器
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // 渲染到 div
+  screenDom.value.appendChild(renderer.domElement);
+
+  // 星空背景
+  const url = require("./assets/images/bg.jpeg");
+
+  // 紋理
+  const envTexture = new THREE.TextureLoader().load(url);
+  scene.background = envTexture;
+  scene.environment = envTexture;
+
+  // 渲染
+  const loopRender = () => {
+    requestAnimationFrame(loopRender);
+    renderer.render(scene, camera);
+  };
+  loopRender();
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*,
+*:before,
+*:after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #000;
+}
+
+.container {
+  width: 100vw;
+  height: 100vh;
+  transform-origin: 0 0;
+  position: relative;
+}
+
+.screen {
+  width: 100vw;
+  height: 100vh;
+}
+
+.pages {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
